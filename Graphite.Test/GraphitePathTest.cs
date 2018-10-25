f3e90ddfe4a2cba5dc7b9430d6ef5f1d06722d02
Bianco Veigel <bianco.veigel@ahd.de>
2018-10-25 13:50:49 +0200

    migrate csproj

--- Graphite.Test/GraphitePathTest.cs
+++ Graphite.Test/GraphitePathTest.cs
@@ -1,65 +1,64 @@
 ﻿using ahd.Graphite.Base;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace ahd.Graphite.Test
 {
-    [TestClass]
     public class GraphitePathTest
     {
-        [TestMethod]
+        [Fact]
         public void GraphitePath()
         {
             var path = new GraphitePath("metric");
-            Assert.AreEqual("metric", path.Name);
-            Assert.AreEqual("metric", path.ToString());
+            Assert.Equal("metric", path.Name);
+            Assert.Equal("metric", path.ToString());
         }
 
-        [TestMethod]
+        [Fact]
         public void CombineNameTest()
         {
             var path = new GraphitePath("metric");
-            Assert.AreEqual("metric", path.ToString());
+            Assert.Equal("metric", path.ToString());
             path = path.Dot("used");
-            Assert.AreEqual("metric.used", path.ToString());
+            Assert.Equal("metric.used", path.ToString());
             path = path.Wildcard();
-            Assert.AreEqual("metric.used*", path.ToString());
+            Assert.Equal("metric.used*", path.ToString());
             path = path.Wildcard();
-            Assert.AreEqual("metric.used*", path.ToString());
+            Assert.Equal("metric.used*", path.ToString());
             path = path.DotWildcard();
-            Assert.AreEqual("metric.used*.*", path.ToString());
+            Assert.Equal("metric.used*.*", path.ToString());
             path = path.Range('a', 'z');
-            Assert.AreEqual("metric.used*.*[a-z]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z]", path.ToString());
             path = path.Range('0', '9');
-            Assert.AreEqual("metric.used*.*[a-z0-9]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9]", path.ToString());
             path = path.DotRange('0', '9');
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9]", path.ToString());
             path = path.Chars('a', 'd', 'f');
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adf]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adf]", path.ToString());
             path = path.Chars('q');
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adfq]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adfq]", path.ToString());
             path = path.DotChars('w');
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adfq].[w]", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adfq].[w]", path.ToString());
             path = path.Values("asdf");
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf}", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf}", path.ToString());
             path = path.Values("qwertz");
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf,qwertz}", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf,qwertz}", path.ToString());
             path = path.DotValues("01","02","03");
-            Assert.AreEqual("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf,qwertz}.{01,02,03}", path.ToString());
+            Assert.Equal("metric.used*.*[a-z0-9].[0-9][adfq].[w]{asdf,qwertz}.{01,02,03}", path.ToString());
         }
 
-        [TestMethod]
+        [Fact]
         public void AlternatingTest()
         {
             var path = new GraphitePath("dummy");
             path = path.Chars('a', 'b', 'd').Values("a","v","b").Chars('z','y','x');
-            Assert.AreEqual("dummy[abd]{a,v,b}[zyx]", path.ToString());
+            Assert.Equal("dummy[abd]{a,v,b}[zyx]", path.ToString());
         }
 
-        [TestMethod]
+        [Fact]
         public void WildcardTest()
         {
             var metric = new GraphitePath("usage").Dot("unittest").DotWildcard().Dot("count");
-            Assert.AreEqual("usage.unittest.*.count", metric.ToString());
+            Assert.Equal("usage.unittest.*.count", metric.ToString());
         }
     }
 }
---
+++ Graphite/HttpClientExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ahd.Graphite
+{
+    internal static class HttpClientExtensions
+    {
+        private static readonly JsonSerializer Serializer = new JsonSerializer();
+
+        internal static async Task<T> ReadAsAsync<T>(this HttpContent content, CancellationToken cancellationToken)
+        {
+            using (var stream = await content.ReadAsStreamAsync().ConfigureAwait(false))
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                using (var sReader = new StreamReader(stream))
+                using (var jReader = new JsonTextReader(sReader))
+                {
+                    return Serializer.Deserialize<T>(jReader);
+                }
+            }
+        }
+    }
+}
